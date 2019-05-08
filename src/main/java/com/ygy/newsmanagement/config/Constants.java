70ecb03fb133ecaa3c4cc7fad3ab011863412ee2
1067649786 <1067649786@qq.com>
2019-05-08 17:37:27 +0800

    newsManagement

---
+++ src/main/java/com/ygy/newsmanagement/config/Constants.java
@@ -0,0 +1,7 @@
+package com.ygy.newsmanagement.config;
+
+public class Constants {
+
+    public final static String FILE_UPLOAD_PATH = "D:\\ygy\\springboot咨询管理信息系统\\newsmanagement\\upload";//上传文件的保存地址，根据部署设置自行修改
+
+}
---
+++ src/main/java/com/ygy/newsmanagement/service/impl/NewsServiceImpl.java
@@ -0,0 +1,63 @@
+package com.ygy.newsmanagement.service.impl;
+
+import com.ygy.newsmanagement.dao.NewsMapper;
+import com.ygy.newsmanagement.entity.News;
+import com.ygy.newsmanagement.service.NewsService;
+import com.ygy.newsmanagement.util.PageQueryUtil;
+import com.ygy.newsmanagement.util.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class NewsServiceImpl implements NewsService {
+
+    @Autowired
+    private NewsMapper newsMapper;
+
+    @Override
+    public String saveNews(News news) {
+        if (newsMapper.insertSelective(news) > 0) {
+            return "success";
+        }
+        return "保存失败";
+    }
+
+    @Override
+    public PageResult getNewsPage(PageQueryUtil pageUtil) {
+        List<News> newsList = newsMapper.findNewsList(pageUtil);
+        int total = newsMapper.getTotalNews(pageUtil);
+        PageResult pageResult = new PageResult(newsList, total, pageUtil.getLimit(), pageUtil.getPage());
+        return pageResult;
+    }
+
+    @Override
+    public Boolean deleteBatch(Integer[] ids) {
+        return newsMapper.deleteBatch(ids)>0;
+    }
+
+    @Override
+    public News queryNewsById(Long newsId) {
+        return newsMapper.selectByPrimaryKey(newsId);
+    }
+
+    @Override
+    public String updateNews(News news) {
+        News newsForUpdate = newsMapper.selectByPrimaryKey(news.getNewsId());
+        if (newsForUpdate == null) {
+            return "数据不存在";
+        }
+        news.setNewsCategoryId(news.getNewsCategoryId());
+        news.setNewsContent(news.getNewsContent());
+        news.setNewsCoverImage(news.getNewsCoverImage());
+        news.setNewsStatus(news.getNewsStatus());
+        news.setNewsTitle(news.getNewsTitle());
+        news.setUpdateTime(new Date());
+        if (newsMapper.updateByPrimaryKeySelective(news) > 0) {
+            return "success";
+        }
+        return "修改失败";
+    }
+}
